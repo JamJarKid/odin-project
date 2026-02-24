ad4db950c669ef99d2215ef15bc55f8e6daeaf2f
JamJarKid <41331314+Malijah-C@users.noreply.github.com>
2026-02-24 23:04:59 +0000

    getting back into odin-project

--- sandbox/src/Person.jsx
+++ sandbox/src/Person.jsx
@@ -1,19 +1,38 @@
 export default function Person() {
-    const [person, setPerson] = useState({ firstName: "John", lastName: "Wick", age: 100 });
+  const [person, setPerson] = useState({
+    firstName: "John",
+    lastName: "Wick",
+    age: 100,
+  });
 
-    const handleIncreaseAge = () => {
-        // copy the existing person object into a new object
-        // while updating the age property
-        const newPerson = { ...person, age: person.age + 1 };
-        setPerson(newPerson);
-    };
+  const handleIncreaseAge = () => {
+    // copy the existing person object into a new object
+    // while updating the age property
+    const newPerson = { ...person, age: person.age + 1 };
+    setPerson(newPerson);
+  };
 
-    return (
-        <>
-            <h1>{person.name}</h1>
-            <h2>{person.age}</h2>
-            <button onClick={handleIncreaseAge}>Increase age</button>
-        </>
-    );
+  return (
+    <div>
+      <input
+        type="text"
+        value={person.firstName}
+        onChange={(event) =>
+          setPerson({ ...person, firstName: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        value={person.lastName}
+        onChange={(event) =>
+          setPerson({ ...person, lastName: event.target.value })
+        }
+      />
+      <h1>
+        {person.firstName} {person.lastName}
+      </h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+    </div>
+  );
 }
-
